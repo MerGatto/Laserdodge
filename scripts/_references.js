a3fabea6cc5e190419df3b576eccfe24ef8dfa46
MerGatto <kepplinger.harald@gmail.com>
2016-02-14 10:51:21 +0100

    Added jsconfig.json
    Added jsconfig
    Removed _references

--- scripts/_references.js
+++
@@ -1,7 +0,0 @@
-///<autosync enabled="true" />
-///<reference path="config.js" />
-///<reference path="controls.js" />
-///<reference path="debug.js" />
-///<reference path="laserdodge.js" />
-///<reference path="menus.js" />
-///<reference path="timer.js" />
